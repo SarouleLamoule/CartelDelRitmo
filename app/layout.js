108aa3fa29e8f6eb4988d2f12f4f3c4e6db21222
SarouleLamoule <chaussat.tim@gmail.com>
2025-04-22 06:09:14 +0200

    add icon

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,19 @@ import "./styles/globals.css";
 export const metadata = {
   title: "Cartel del Ritmo",
   description: "Le son de Nuevo Mexico",
+  icons: {
+    icon: "/logo.ico",
+    shortcut: "/logo.ico",
+    apple: "/logo.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="icon" href="/logo.ico" type="image/x-icon" />
+      </head>
       <body>
         <Header />
         <main className="main-content">{children}</main>
